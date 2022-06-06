18e652c672afb8a4f602d4a80b9c726acffdf16f
Dane Kim <mr.dane@danes-mbp.lan>
2022-06-06 16:23:36 -0400

    setting up class names for all layouts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
-import "./App.css";
+import "./components/styles.css";
+import SideNav from "./components/SideNav";
+import Reviews from "./components/Reviews";
+import AvgRtg from "./components/AvgRtg";
+import Sentiment from "./components/Sentiment";
+import WebVisit from "./components/WebVisit";
+import WebLayout from "./components/WebLayout";
 
 function App() {
-  return <div className="main"></div>;
+  return (
+    <div className="container">
+      <div className="main"></div>
+      <div className="Nav">
+        <SideNav />
+      </div>
+      <div className="Reviews">
+        <Reviews />
+      </div>
+      <div className="AvgRtg">
+        <AvgRtg />
+      </div>
+      <div className="SentAnalysis">
+        <Sentiment />
+      </div>
+      <div className="web-visitors">
+        <WebVisit />
+      </div>
+      <div className="web-layout">
+        <WebLayout />
+      </div>
+    </div>
+  );
 }
 
 export default App;
